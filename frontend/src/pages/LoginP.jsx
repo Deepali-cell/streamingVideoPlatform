22cab399b1eea17a2d4b861cdd7b4577f94c0be2
Deepali <joysingal247@gmail.com>
2026-01-11 15:02:53 +0530

    case sensitive issue

--- frontend/src/pages/LoginP.jsx
+++ frontend/src/pages/LoginP.jsx
@@ -8,8 +8,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
-import { useAppContext } from "@/context/stateContext";
 import axios from "axios";
+import { useAppContext } from "@/context/StateContext";
 
 const LoginP = () => {
   const { backend_url, navigate, setisUser } = useAppContext();
